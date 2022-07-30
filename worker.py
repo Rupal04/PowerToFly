a077e493eb2192771aba39b7a97769301a84db95
rupal <rupal.gupta@innovaccer.com>
2022-07-31 01:20:44 +0530

    Changed worker

--- worker.py
+++ worker.py
@@ -1,18 +1,13 @@
 import os
+
 import redis
-import urllib.parse as urlparse
-from redis import Redis
 from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/1')
 
-urlparse.uses_netloc.append('redis')
-url = urlparse.urlparse(redis_url)
-conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
-
-# conn = redis.from_url(redis_url)
+conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
